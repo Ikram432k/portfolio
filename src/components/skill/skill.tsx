d412ccc55e26c8dd6d6493b815da52e0dd0ea294
ikram mohideen <ikram432k@gmail.com>
2022-06-21 18:11:45 +0530

    src/components/skill/skill.tsx

--- src/components/skill/skill.tsx
+++ src/components/skill/skill.tsx
@@ -1,6 +1,21 @@
+import { Section } from "./skillStyle";
+import { data } from "../data/icons";
 const Skill =()=>{
     return(
-        <section id="skills">skills</section>
+        <Section id="skills" className="">
+            <h5>What i have Learned</h5>
+            <h2>skills</h2>
+            <div className="container project_container">
+                {data.map((obj,i)=>{
+                    return(
+                        <article className="project_item" key={i}>
+                            <img src={obj.path} alt="" />
+                            <h3>{obj.title}</h3>
+                    </article>
+                    );
+                })}
+            </div>
+        </Section>
     )
 }
 export default Skill;
