1f6266c03ec9bddaf712237e6e8905f0cd4050eb
ikram mohideen <ikram432k@gmail.com>
2022-06-21 18:09:47 +0530

    src/components/contact/contactStyle.tsx

--- src/components/contact/contactStyle.tsx
+++ src/components/contact/contactStyle.tsx
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 export const Section = styled.section`
-height: 100vh;
+height: 75vh;
 padding-top: 7rem;
 overflow: hidden;
-.contact_container{
 
-}
 .card_container{
+    position: relative;
     display:flex;
     justify-content: center;
     align-items: center;
@@ -15,11 +14,25 @@ overflow: hidden;
 .contact_card{
     background: var(--color-bg-variant);
     border: 1px solid transparent;
-    border-radius: 1rem;
-    padding: 2rem;
+    border-radius: 0.5rem;
+    padding: 1rem;
     text-align: center;
 }
-.icon{
-    width:;
+.scroll_up{
+    position: absolute;
+    right: -1rem;
+    bottom: 5rem;
+    transform: rotate(90deg);
+    font-weight: 300;
+    font-size: 0.9rem;
+}
+@media screen and (max-width: 600px){
+.card_container{
+    display:flex;
+    // flex-direction: column;
+}
+.scroll_up{
+    display:none;
+}
 }
 `;
