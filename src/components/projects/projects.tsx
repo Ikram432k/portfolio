a34e28882f47b5ebcd37f11da9c1333d1ee6cdbe
ikram mohideen <ikram432k@gmail.com>
2022-06-21 18:11:05 +0530

    src/components/projects/projects.tsx

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,6 +1,27 @@
+import { projectData } from "../data/projectsData";
+import { Section } from "./projectsStyle";
 const Project =()=>{
     return(
-        <section id="projects">projects</section>
+        <Section id="projects" className="">
+            <h5>My recent Works</h5>
+            <h2>projects</h2>
+            <div className="container project_container">
+                {projectData.map((obj,i)=>{
+                    return(
+                        <article className="project_item" key={i}>
+                        <div className="project_img">
+                            <img src={obj.path} alt="" />
+                        </div>
+                        <h3>{obj.title}</h3>
+                        <div className="project_item-btn">
+                        <a href={obj.code} className="btn">View code</a>
+                        <a href={obj.demo} className="btn btn-primary">Live Demo</a>
+                        </div>
+                    </article>
+                    );
+                })}
+            </div>
+        </Section>
         )
 }
 export default Project;
