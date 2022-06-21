132405d1b4452754a8263f7bcb90d9e61e923a8a
ikram mohideen <ikram432k@gmail.com>
2022-06-21 18:09:27 +0530

    src/components/contact/contact.tsx

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -3,21 +3,22 @@ import {IoMdMail} from 'react-icons/io';
 import {Section} from "./contactStyle";
 const Contact=()=>{
     return(
-        <Section id="contact" className="container contact_container ">
+        <Section id="contact" className="container contact_container">
             <h2>Contact</h2>
             <div className="card_container">
                 <div className='contact_card'>
-                <a href="" target="_blank"><BsLinkedin className='icon'/></a>
+                <a href="https://www.linkedin.com/in/ikram-mohideen-235313229/" target="_blank" rel="noreferrer" ><BsLinkedin size={50}/></a>
                 <h4>Linkedin</h4>
                 </div>
                 <div className='contact_card'>
-                <a href="" target="_blank"><IoMdMail/></a>
+                <a href="mailto:ikram432k@gmail.com"><IoMdMail size={50}/></a>
                 <h4>Gmail</h4>
                 </div>
                 <div className='contact_card'>
-                <a href="" target="_blank"><BsTwitter/></a>
+                <a href="https://twitter.com/IkramMohideen1" target="_blank" rel='noreferrer'><BsTwitter size={50}/></a>
                 <h4>Twitter</h4>
                 </div>
+                <a href="#" className="scroll_up">Scroll up</a> 
             </div>
         </Section>
     )
